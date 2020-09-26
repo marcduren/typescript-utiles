cb201a11ea4b30a850539a1a1ebd3919657dd3e4
Marc DUREN <marc.duren@mdsp.fr>
2020-09-26 17:54:20 +0200

    add objects

--- strings.ts
+++ strings.ts
@@ -1,3 +1,10 @@
-export function padEnd (str: string, length: number, char = '0') {
+export function padEnd(str: string, length: number, char = '0') {
   return str + char.repeat(Math.max(0, length - str.length))
+}
+
+/**
+ * Makes the first character of a string uppercase
+ */
+export function upperFirst(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
